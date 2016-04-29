1e2c518334555b067bfb52d711de9608ae2a9842
RainbowPeach <J.niklas.klocke@gmail.com>
2016-04-29 11:36:36 +0200

    initial fields for DatatRepository

---
+++ AtgVerwaltung/AtgVerwaltung.Lib/DataRepository.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AtgVerwaltung.Model;
+
+namespace AtgVerwaltung.Lib
+{
+    class DataRepository
+    {
+        #region props
+        public List<Kunde> Kunden { get; set; }
+        public List<Artikel> Artikel { get; set; }
+        public List<Auftrag> Auftaege { get; set; }
+        public List<Auftragsposition> Auftragspositionen { get; set; }
+        #endregion
+
+        #region constructors
+
+        public DataRepository()
+        {
+            
+        }
+        #endregion
+
+        public string ErstelleKunde()
+        {
+            throw new NotImplementedException();
+            //ToDO
+        }
+
+        public string ErstelleAuftrag(Kunde kunde)
+        {
+            throw new NotImplementedException();
+            //ToDO
+        }
+
+        public string ErstelleAzftragPosition(Auftrag auftrag)
+        {
+            throw new NotImplementedException();
+            //ToDO
+        }
+
+        public string ErstelleAkrtikel()
+        {
+            throw new NotImplementedException();
+            //ToDO
+        }
+
+    }
+}
